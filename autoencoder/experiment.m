385e92684389228516e0b8260b939b25a75c93f1
Justin Johnson <jcjohns@stanford.edu>
2013-02-20 21:50:28 -0800

    Final progress report

--- autoencoder/experiment.m
+++ autoencoder/experiment.m
@@ -14,10 +14,19 @@ numTrainingExamples = size(X, 2);
 % data = randn(numTrainingExamples, 2) * R;
 % data = data';
 
-hiddenSize = 25;
-lambda = 0.01;
-beta = 10;
+hiddenSize = 196;
+lambda = 0;
+beta = 0;
 rho = 0.1;
+
+% Lets do PCA
+% Mean subtraction:
+Xc = X - repmat(mean(X, 2), 1, numTrainingExamples);
+[U, S, V] = svd(Xc, 0);
+U = U(:, 1:hiddenSize);
+display_network(U);
+
+pause;
 
 theta = initializeParameters(hiddenSize, inputSize);
 
@@ -25,7 +34,7 @@ addpath minFunc/
 options.Method = 'lbfgs';
 options.maxIter = 200;
 options.display = 'on';
-[opttheta, cost] = minFunc(@(p) sparseLinearAutoencoderCost(p, inputSize, hiddenSize, lambda, rho, beta, data), ...
+[opttheta, cost] = minFunc(@(p) sparseAutoencoderCostVectorized(p, inputSize, hiddenSize, lambda, rho, beta, data), ...
                            theta, options);
                        
 [W1, W2, b1, b2] = unrollParameters(opttheta, inputSize, hiddenSize);
